eb27e45b63fc2a17ebdd93a1160a46a31e8f9de9
ygm9960-spec <ygm9960@gmail.com>
2026-08-28 12:56:10 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Google Apps Script 웹앱 배포 후 /exec URL을 아래 따옴표 안에 붙여 넣으세요.
+// 예: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+window.APP_CONFIG = {
+  GOOGLE_SHEETS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxw7uZFKUssDePanIpZfPb-f9xn9bxKNVQfmN7ZoC20QKd7o2Ch4GU4nSjB8iBeAtkPcA/exec'
+};
